676e7e22195ec610e1480f817b78bbd0e3fb9b5f
Foshati <amirrezafoshati@gmail.com>
2025-01-07 13:49:44 +0330

    create authentication full for next.js and prisma and better-auth

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.externals.push("@libsql/client");
+    return config;
+  },
+  output: "standalone",
+};
+
+export default nextConfig;
