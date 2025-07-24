1c11c7cac83828b7d0f9ce832fb303ce45b4cc40
foshati <amirrezafoshati@gmail.com>
2025-07-24 23:59:50 +0330

    fix lint 1

--- app/(auth)/_lib/helper/client-utility.ts
+++ app/(auth)/_lib/helper/client-utility.ts
@@ -1,6 +1,7 @@
-import { client } from "../auth-client";
+// /* eslint-disable @typescript-eslint/no-unsafe-return */
+// import { client } from "../auth-client";
 
-export async function getClientSession() {
-  const session = await client.getSession();
-  return session;
-}
+// export async function getClientSession() {
+//   const session = await client.getSession();
+//   return session;
+// }
